711ba67f1412c26cc497ece8e80133f49c8963f0
Morhaf Alaraj <morhaf@student.chalmers.se>
2015-01-28 09:48:24 +0100

    Deleted lib not needed

--- ParseChart/front/src/polyfill.js
+++
@@ -1,7 +0,0 @@
-// fix problems using c3 with phantomjs #578
-Function.prototype.bind = Function.prototype.bind || function (thisp) {
-    var fn = this;
-    return function () {
-        return fn.apply(thisp, arguments);
-    };
-};
